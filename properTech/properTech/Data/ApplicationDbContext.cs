6ee2f1dbd268119b9cc04dc926cd97b251d5b7e8
Charles Clark <ctcb57@gmail.com>
2019-09-16 08:19:08 -0500

    Initial push

---
+++ properTech/properTech/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using properTech.Models;
+
+namespace properTech.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
