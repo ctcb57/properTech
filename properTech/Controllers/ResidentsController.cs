b73104788311028f9880ffc3f07a8396a8ae2ec9
Charles Clark <ctcb57@gmail.com>
2019-09-16 14:28:44 -0500

    Resident and MaintenanceTech controllers uploaded

---
+++ properTech/Controllers/ResidentsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using properTech.Data;
+using properTech.Models;
+
+namespace properTech.Controllers
+{
+    public class ResidentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ResidentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Residents
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Resident.Include(r => r.unit);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Residents/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resident = await _context.Resident
+                .Include(r => r.unit)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (resident == null)
+            {
+                return NotFound();
+            }
+
+            return View(resident);
+        }
+
+        // GET: Residents/Create
+        public IActionResult Create()
+        {
+            ViewData["unitId"] = new SelectList(_context.Set<Unit>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Residents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,firstName,lastName,leaseStart,leaseSEnd,renewedLease,paymentDueDate,latePayment,balance,userId,unitId")] Resident resident)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(resident);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["unitId"] = new SelectList(_context.Set<Unit>(), "Id", "Id", resident.unitId);
+            return View(resident);
+        }
+
+        // GET: Residents/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resident = await _context.Resident.FindAsync(id);
+            if (resident == null)
+            {
+                return NotFound();
+            }
+            ViewData["unitId"] = new SelectList(_context.Set<Unit>(), "Id", "Id", resident.unitId);
+            return View(resident);
+        }
+
+        // POST: Residents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,firstName,lastName,leaseStart,leaseSEnd,renewedLease,paymentDueDate,latePayment,balance,userId,unitId")] Resident resident)
+        {
+            if (id != resident.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(resident);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ResidentExists(resident.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["unitId"] = new SelectList(_context.Set<Unit>(), "Id", "Id", resident.unitId);
+            return View(resident);
+        }
+
+        // GET: Residents/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resident = await _context.Resident
+                .Include(r => r.unit)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (resident == null)
+            {
+                return NotFound();
+            }
+
+            return View(resident);
+        }
+
+        // POST: Residents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var resident = await _context.Resident.FindAsync(id);
+            _context.Resident.Remove(resident);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ResidentExists(int id)
+        {
+            return _context.Resident.Any(e => e.Id == id);
+        }
+    }
+}
