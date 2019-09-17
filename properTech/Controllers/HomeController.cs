9acc2d86c617bd44a3febe7a581f363adc043962
iaiir <abeyang13@gmail.com>
2019-09-17 15:48:04 -0500

    created a Vacancies method and UnitDetails method in the HomeController.

--- properTech/Controllers/HomeController.cs
+++ properTech/Controllers/HomeController.cs
@@ -4,12 +4,19 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using properTech.Data;
 using properTech.Models;
 
 namespace properTech.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -23,7 +30,24 @@ namespace properTech.Controllers
         //Get all information on Vacancies 
         public IActionResult Vacancies()
         {
-            return View();
+            return View(_context.Unit.Where(u=> u.IsOccupied == false).ToList());
+        }
+
+        //Get Details of Unit that is open
+        public IActionResult UnitDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var unit = _context.Unit.Find(id);
+            if (unit == null)
+            {
+                return NotFound();
+            }
+
+            return View(unit);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
